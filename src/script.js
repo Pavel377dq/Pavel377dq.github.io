aaaa6b72585e85b6b7cb325aa3719adf1b7741b9
Pavel Poryvai <42907367+Pavel377dq@users.noreply.github.com>
2023-03-10 02:19:22 +0300

    1.6 done

--- src/script.js
+++ src/script.js
@@ -114,8 +114,120 @@ window.addEventListener("resize", function () {
 
 }
 
+function ListenersPopUp(){
+
+  let buttonBurger = document.querySelector('.header__wrap-margin-left .button');
+  let popUpMenu = document.querySelector('.pop-up-menu');
+  let buttonClose = document.querySelector('.pop-up-menu__wrap .button')
+ 
+ 
+  buttonBurger.addEventListener('click', e => {
+    e.stopPropagation();
+  
+    popUpMenu.classList.add('pop-up-menu--visible');
+  });
+
+  buttonClose.addEventListener('click', e => {
+    e.stopPropagation();
+  
+    popUpMenu.classList.remove('pop-up-menu--visible');
+  });
+  
+  document.addEventListener('click', e => {
+   
+    const withinBoundaries = e.composedPath().includes(popUpMenu);
+ 
+	if ( ! withinBoundaries && popUpMenu.classList.contains('pop-up-menu--visible')) {
+    popUpMenu.classList.remove('pop-up-menu--visible');
+	}
+  })
+
+
+}
+
+
+function ListenersCallMessage(icon,closeSmallWidth, closeBigWidth, modalWindow, modalWindowMod){
+
+  let buttonOpen = document.querySelector(icon);
+  let buttonCloseSmall = document.querySelector(closeSmallWidth);
+  let buttonCloseBig = document.querySelector(closeBigWidth);
+  let modal = document.querySelector(modalWindow);
+
+  buttonOpen.addEventListener('click', function(evt){
+
+    evt.preventDefault();
+    evt.stopPropagation();
+    modal.classList.add(modalWindowMod);
+
+  });
+
+  buttonCloseSmall.addEventListener('click', evt => {
+    evt.preventDefault();
+    evt.stopPropagation();
+  
+    modal.classList.remove(modalWindowMod);
+  });
+
+
+  
+  buttonCloseBig.addEventListener('click', evt => {
+    evt.preventDefault();
+    evt.stopPropagation();
+  
+    modal.classList.remove(modalWindowMod);
+  });
+
+
+
+  document.addEventListener('click', evt => {
+   
+    const withinBoundaries = evt.composedPath().includes(modal);
+ 
+	if ( ! withinBoundaries && modal.classList.contains(modalWindowMod)) {
+    modal.classList.remove(modalWindowMod);
+	}
+  })
+
+
+
+
+}
+
+function ListenerMainText(){
+
+  let expandButton = document.querySelector('.page-service__text .expand');
+  let icon = document.querySelector('.page-service__text .expand__icon')
+  let mainText = document.querySelector('.page-service__text-formation');
+  let text = document.querySelector('.page-service__text .expand__text')
+  let isOpen=0;
+  expandButton.addEventListener('click',evt => {
+
+    expandButton.classList.toggle('expand-margin-top');
+    icon.classList.toggle('expand__icon-rotate');
+    mainText.classList.toggle('page-service__text-formation--visible');
+    if(isOpen){
+      text.textContent = 'Читать далее';
+      isOpen = 0;
+    }
+    else{
+      isOpen = 1;
+      text.textContent = 'Скрыть';
+    }
+
+  })
+}
+
+
 
 Listeners('.repair-elements__button-wrapper .expand','.repair-elements__button-wrapper .expand__text','.repair-elements__button-wrapper .expand__icon','.repair-elements__list','repair-elements__list--visible','repair-elements__expand--margin-top-small','repair-elements__expand--margin-top-big','article.products-repair','products-repair--top-margin');
 
 Listeners('.technique-elements__button-wrapper .expand','.technique-elements__button-wrapper .expand__text','.technique-elements__button-wrapper .expand__icon','.technique-elements__list','technique-elements__list--visible','technique-elements__expand--margin-top-small','technique-elements__expand--margin-top-big','section.products-repair','products-repair--top-margin');
 
+ListenersPopUp();
+
+ListenersCallMessage('.list-buttons__button .button__phone-icon','.request-call__button','.modals__call .clean-list__button','.modals__call','modals__call--visible');
+
+
+ListenersCallMessage('.list-buttons__button .button__message-icon','.request-message__button','.modals__message .clean-list__button','.modals__message','modals__message--visible');
+
+ListenerMainText();
